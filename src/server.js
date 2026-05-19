acafa8413e30f2f17feb466aad3b76d28ab056a4
Vitalina <vsinkovaa@gmail.com>
2026-05-19 18:14:59 +0300

    middleware_and_routing_commits

--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import { pinoHttp } from 'pino-http';
+// Завантажує змінні середовища
 import 'dotenv/config';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Middleware -дозволяє робити запити з інших доменів
 app.use(cors());
 
+// Middleware - дозволяє обробляти дані у форматі JSON, які надходять у body запиту
 app.use(express.json());
 
+// Middleware - інтегрує логування HTTP-запитів
 app.use(pinoHttp());
+
+// GET зареєструвати обробник маршрута (повертає всі маршрути)
+// app.get(шлях, ф-ція-обробник/контролер)
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
@@ -19,6 +26,7 @@ app.get('/notes', (req, res) => {
   });
 });
 
+// Повертає одну нотатку за її ідентифікатором
 app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
 
@@ -27,10 +35,12 @@ app.get('/notes/:noteId', (req, res) => {
   });
 });
 
+// спеціальний тестовий маршрут для імітації виникнення помилки
 app.get('/test-error', () => {
   throw new Error('Simulated server error');
 });
 
+// Мiddleware для обробки всіх запитів, що не відповідають жодному наявному маршруту
 app.use((req, res) => {
   res.status(404).json({
     message: 'Route not found',
@@ -40,6 +50,7 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   const isProd = process.env.NODE_ENV === 'production';
 
+  // Мiddleware що перехоплює помилки
   res.status(500).json({
     message: isProd ? 'Server error' : err.message,
   });
